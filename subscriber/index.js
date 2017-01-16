bc447f11080283a3924f3da3448fc55a542e4f31
Gustavo Covas <gcovas@topster.com.br>
2017-01-16 13:56:08 -0200

    Add docker compose

--- subscriber/index.js
+++ subscriber/index.js
@@ -5,7 +5,7 @@ var zmq = require('zeromq')
 sub.connect('tcp://192.168.99.100:3001');
 sub.subscribe('PETR4');
 
-console.log('Worker connected to port 3000');
+console.log('Worker connected to port 3001');
 
 sub.on('message', function(topic, message){
   console.log('received a message related to:', topic.toString(), 'containing message:', message.toString());
