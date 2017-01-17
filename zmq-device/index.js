eec98e9dc9db92f5738ddc18cf0c15642f2e1114
Gustavo Covas <gcovas@topster.com.br>
2017-01-17 11:06:35 -0200

    Change device socket types to xpub and xsub

--- zmq-device/index.js
+++ zmq-device/index.js
@@ -1,15 +1,21 @@
 // ticker
 var zmq = require('zeromq')
-  , pub = zmq.socket('pub')
-  , sub = zmq.socket('sub');
+  , pub = zmq.socket('xpub')
+  , sub = zmq.socket('xsub');
 
-sub.subscribe('message');
+// sub.subscribe('message');
 sub.bindSync('tcp://*:3001');
 console.log('Device bound to port 3001');
 
+pub.setsockopt(zmq.ZMQ_XPUB_VERBOSE, 1);
 pub.bindSync('tcp://*:3002');
 
 sub.on('message', function(topic, message) {
-   console.log('Repassing message related to:', topic.toString(), 'containing message:', message.toString());
-   pub.send([topic, message]);
+  console.log('Repassing message related to:', topic.toString(), 'containing message:', message.toString());
+  pub.send([topic, message]);
 });
+
+pub.on('message', function(data) {
+  sub.send(data);
+  console.log('pub sending to sub...');
+})
