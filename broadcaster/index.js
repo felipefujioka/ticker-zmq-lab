ecddf865b0f708c997e62568022a821e94adc5ee
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-18 18:42:59 -0200

    multi proxies almost there

--- broadcaster/index.js
+++ broadcaster/index.js
@@ -6,10 +6,10 @@ var io = require('socket.io')();
 io.on('connection', function(client){ });
 io.listen(process.env.PORT);
 
-sub.connect('tcp://192.168.99.100:3002');
+sub.connect('tcp://192.168.99.100:' + process.env.SUB_PORT);
 sub.subscribe(process.env.CHANNEL);
 
-console.log('Worker connected to port 3002');
+console.log('Worker connected to port ' + process.env.SUB_PORT);
 
 sub.on('message', function(topic, timestamp, message){
   console.log('received a message related to:', topic.toString(), 'containing message:', message.toString());
--- zmq-proxy/src/main/java/com/topster/zmqproxy/App.java
+++ zmq-proxy/src/main/java/com/topster/zmqproxy/App.java
@@ -2,6 +2,7 @@ package com.topster.zmqproxy;
 
 import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
 
 public class App {
@@ -10,14 +11,62 @@ public class App {
 
         //  Socket facing clients
         Socket sub = context.socket(ZMQ.XSUB);
-        sub.bind("tcp://*:3001");
+        sub.bind("tcp://*:" + System.getenv("SUB_PORT"));
 
         //  Socket facing services
         Socket pub = context.socket(ZMQ.XPUB);
-        pub.bind("tcp://*:3002");
+        pub.setXpubVerbose(true);
+        pub.bind("tcp://*:" + System.getenv("PUB_PORT"));
+
+        Socket backupPub = context.socket(ZMQ.PUB);
+        backupPub.connect("tcp://192.168.99.100:" + System.getenv("NEXT_PORT_PUB"));
+
+        Socket backupSub = context.socket(ZMQ.PUB);
+        backupSub.connect("tcp://192.168.99.100:" + System.getenv("NEXT_PORT_SUB"));
+
+        Poller items = new Poller (2);
+        items.register(sub, Poller.POLLIN);
+        items.register(pub, Poller.POLLIN);
 
         //  Start the proxy
-        ZMQ.proxy (sub, pub, null);
+        // ZMQ.proxy (sub, pub, backup);
+
+        boolean more = false;
+        byte[] message;
+
+        while (!Thread.currentThread().isInterrupted()) {            
+            //  poll and memorize multipart detection
+            items.poll();
+
+            if (items.pollin(0)) {
+                while (true) {
+                    // receive message
+                    message = sub.recv(0);
+                    more = sub.hasReceiveMore();
+                    System.out.println("incoming: " + new String(message));
+                    // Broker it
+                    pub.send(message, more ? ZMQ.SNDMORE : 0);
+                    backupPub.send(message, more ? ZMQ.SNDMORE : 0);
+                    if(!more){
+                        break;
+                    }
+                }
+            }
+            if (items.pollin(1)) {
+                while (true) {
+                    // receive message
+                    message = pub.recv(0);
+                    more = pub.hasReceiveMore();
+                    System.out.println("outcoming: " + new String(message));
+                    // Broker it
+                    sub.send(message,  more ? ZMQ.SNDMORE : 0);
+                    backupSub.send(message, more ? ZMQ.SNDMORE : 0);
+                    if(!more){
+                        break;
+                    }
+                }
+            }
+        }
 
         //  We never get here but clean up anyhow
         sub.close();
