6a32058a304fd98e40903da55a38c567ec8c515f
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-18 11:34:13 -0200

    proxy docker image building

---
+++ zmq-proxy/src/main/java/com/topster/zmqproxy/App.java
@@ -0,0 +1,27 @@
+package com.topster.zmqproxy;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+public class App {
+  public static void main( String[] args) {
+        Context context = ZMQ.context(1);
+
+        //  Socket facing clients
+        Socket sub = context.socket(ZMQ.XSUB);
+        sub.bind("tcp://*:3000");
+
+        //  Socket facing services
+        Socket pub = context.socket(ZMQ.XPUB);
+        pub.bind("tcp://*:3001");
+
+        //  Start the proxy
+        ZMQ.proxy (sub, pub, null);
+
+        //  We never get here but clean up anyhow
+        sub.close();
+        pub.close();
+        context.term();
+  }
+}
