72f6a1ac847f74944b1ae62618959ca75f7f43cd
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-20 13:13:03 -0200

    functional java ticker

--- java-ticker/src/main/java/com/mycompany/javaticker/App.java
+++ java-ticker/src/main/java/com/mycompany/javaticker/App.java
@@ -11,36 +11,47 @@ import com.google.gson.Gson;
 public class App 
 {
 
+  static Integer seq = 0;
+
   private static class Message {
     String price;
     String seq;
+
+    public Message(Double price, Integer seq) {
+      this.price = price.toString();
+      this.seq = seq.toString();
+
+    }
   }
 
-    public static void main( String[] args )
+  public static Double addRandomVariationTo(String price) {
+    Double fPrice = Double.parseDouble(price);
+    Double signal = Math.random() > 0.5 ? -1.0 : 1.0;
+    Double variation = Math.random();
+    return fPrice + signal*variation;
+  }
+
+  public static void main( String[] args )
     {
       Context context = ZMQ.context(1);
 
-      //  Socket facing publishers
       Socket pub = context.socket(ZMQ.PUB);
+      System.out.println("tcp://192.168.99.100:" + System.getenv("PUB_PORT"));
       pub.connect("tcp://192.168.99.100:" + System.getenv("PUB_PORT"));
 
       while(true) {
         System.out.println("sending tick to " + System.getenv("SECURITY_SYMBOL"));
-        pub.send(System.getenv("SECURITY_SYMBOL").getBytes(), ZMQ.SNDMORE);
-        pub.send(new Date().toString().getBytes(), ZMQ.SNDMORE);
-        Message message = new Message();
-        message.price = "123";
-        message.seq= "1";
-        pub.send(new Gson().toJson(message).getBytes(), 0);
+        pub.send(("TICK." + System.getenv("SECURITY_SYMBOL")).getBytes(), ZMQ.SNDMORE);
+        pub.send(("" + new Date().getTime()).getBytes(), ZMQ.SNDMORE);
+        Message message = new Message(App.addRandomVariationTo(System.getenv("BASE_PRICE")), App.seq++);
+        String msgStr = new Gson().toJson(message);
+        System.out.println("sending message: " + msgStr);
+        pub.send(msgStr.getBytes(), 0);
         try {
           Thread.sleep(Integer.parseInt(System.getenv("TICK_PERIOD")));
         }catch(java.lang.InterruptedException e) {
 
         }
       }
-
-      pub.close();
-
-      context.term();
     }
 }
