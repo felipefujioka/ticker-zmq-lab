4c563cd19d0d10c1035e3cac495b3b455e535f20
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-01-31 18:19:43 -0200

    broadcaster standalone

--- broadcaster/index.js
+++ broadcaster/index.js
@@ -6,18 +6,16 @@ var io = require('socket.io')();
 io.on('connection', function(client){ });
 io.listen(process.env.PORT);
 
-sub.connect('tcp://192.168.99.100:' + process.env.SUB_PORT);
+sub.connect('tcp://' + process.env.SUB_ADDR);
 sub.subscribe(process.env.CHANNEL);
-
-console.log('Worker connected to port ' + process.env.SUB_PORT);
 
 sub.on('message', function(topic, timestamp, message){
   console.log('i: ' + topic + ' ' + timestamp + ' ' + message.toString());
 
-  var wrapper = {
-    topic: topic.toString(),
-    timestamp: timestamp.toString(),
-    message: JSON.parse(message.toString())
-  };
-  io.emit('message', JSON.stringify(wrapper));
+//  var wrapper = {
+//    topic: topic.toString(),
+//    timestamp: timestamp.toString(),
+//    message: JSON.parse(message.toString())
+//  };
+//  io.emit('message', JSON.stringify(wrapper));
 });
