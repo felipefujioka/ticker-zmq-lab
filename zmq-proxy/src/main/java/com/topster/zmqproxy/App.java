2f3377be5701b6bb49db15b95038765bb0d9b3b8
Felipe Jun Fujioka Shida <ffujioka@topster.com.br>
2017-02-06 13:03:47 -0200

    some mods to standalone broadcaster

--- zmq-proxy/src/main/java/com/topster/zmqproxy/App.java
+++ zmq-proxy/src/main/java/com/topster/zmqproxy/App.java
@@ -5,106 +5,98 @@ import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
 
+import java.util.ArrayList;
+
 public class App {
 
-  private enum Destination {
-    SUBSCRIBERS,
-    PUBLISHERS
-  };
+  private static final String SUB_ADDR="tcp://127.0.0.1:9999";
+
+  private final static int IDS = 1;
+  private final static int QNT = 5;
 
   public static void main(String[] args) {
     Context context = ZMQ.context(1);
 
-    //  Socket facing publishers
-    Socket cloudSub = context.socket(ZMQ.XSUB);
-    cloudSub.bind("tcp://*:" + System.getenv("SUB_PORT"));
+    ArrayList<Thread> ths = new ArrayList<Thread>();
 
-    //  Socket facing subscribers
-    Socket cloudPub = context.socket(ZMQ.XPUB);
-    cloudPub.setXpubVerbose(true);
-    cloudPub.bind("tcp://*:" + System.getenv("PUB_PORT"));
+    for(int i = 1; i <= IDS; i++) {
+      for(int j = 1; j <= QNT; j++) {
+        Consumer c = new Consumer(i, j, context);
 
-    Socket localPub = context.socket(ZMQ.PUB);
-    localPub.bind("tcp://*:" + System.getenv("LOCAL_PUB_PORT"));
+        Thread t = new Thread(c);
 
-    Socket localSub = context.socket(ZMQ.SUB);
-    localSub.subscribe("".getBytes());
-    localSub.connect("tcp://192.168.99.100:" + System.getenv("LOCAL_SUB_PORT"));
+        ths.add(t);
 
-    Poller items = new Poller (3);
-    items.register(cloudSub, Poller.POLLIN);
-    items.register(cloudPub, Poller.POLLIN);
-    items.register(localSub, Poller.POLLIN);
+        t.start();
 
-    boolean more = false;
-    byte[] message;
-
-    while (!Thread.currentThread().isInterrupted()) {
-      items.poll();
-
-      if (items.pollin(0)) {
-        localPub.send(Destination.SUBSCRIBERS.name().getBytes(), ZMQ.SNDMORE);
-        while (true) {
-          // receive message
-          message = cloudSub.recv(0);
-          System.out.println("r: " + new String(message));
-          more = cloudSub.hasReceiveMore();
-          // System.out.println("received from publisher: " + new String(message));
-          // Broker it
-          cloudPub.send(message, more ? ZMQ.SNDMORE : 0);
-          localPub.send(message, more ? ZMQ.SNDMORE : 0);
-          if (!more) {
-            break;
-          }
-        }
       }
+    }
 
-      if (items.pollin(1)) {
-        localPub.send(Destination.PUBLISHERS.name().getBytes(), ZMQ.SNDMORE);
-        while (true) {
-          // receive message
-          message = cloudPub.recv(0);
-          System.out.println("r: " + new String(message));
-          more = cloudPub.hasReceiveMore();
-          // Broker it
-          cloudSub.send(message,  more ? ZMQ.SNDMORE : 0);
-          localPub.send(message,  more ? ZMQ.SNDMORE : 0);
-          if (!more) {
-            break;
-          }
-        }
-      }
+    for (int i = 0; i < ths.size(); i++) {
+      try {
+        ths.get(i).join();
+      }catch(InterruptedException e) {
 
-      if (items.pollin(2)) {
-        String destinationStr = new String(localSub.recv(0));
-        Destination destination = Destination.valueOf(destinationStr);
-
-        while (true) {
-          // receive message
-          message = localSub.recv(0);
-          System.out.println("l: " + new String(message));
-          more = localSub.hasReceiveMore();
-          // Broker it
-          if (destination == Destination.SUBSCRIBERS) {
-            cloudPub.send(message,  more ? ZMQ.SNDMORE : 0);
-          } else if (destination == Destination.PUBLISHERS) {
-            cloudSub.send(message,  more ? ZMQ.SNDMORE : 0);
-          }
-          // TODO: else raise Exception ?
-
-          if (!more) {
-            break;
-          }
-        }
       }
     }
 
     //  We never get here but clean up anyhow
-    cloudSub.close();
-    cloudPub.close();
-    localSub.close();
-    localPub.close();
-
     context.term();
   }
+
+  public static class Consumer implements Runnable {
+
+    private int id;
+
+    private int order;
+
+    private Context context;
+
+    public Consumer(int id, int order, Context context) {
+      this.id = id;
+      this.order = order;
+      this.context = context;
+    }
+
+    @Override
+    public void run() {
+
+      //  Socket facing publishers
+      Socket subSocket = context.socket(ZMQ.SUB);
+      subSocket.connect(SUB_ADDR);
+//      if (this.id < 10) {
+//        subSocket.subscribe(("TICK.T0" + this.id).getBytes());
+//      }else {
+//        subSocket.subscribe(("TICK.T" + this.id).getBytes());
+//      }
+
+      subSocket.subscribe("".getBytes());
+
+      boolean more = false;
+      String message;
+      int inc = 0;
+      long initialTime = System.currentTimeMillis();
+
+      while (!Thread.currentThread().isInterrupted()) {
+        while (true) {
+          // receive message
+          message = subSocket.recvStr(0);
+          more = subSocket.hasReceiveMore();
+
+          if (!more) {
+//            inc++;
+
+//            if(inc >= 10000) {
+//              inc = 0;
+//              System.out.println("time to subscriber " + this.id + "-" + this.order + " receive 10000 msgs: " + (System.currentTimeMillis() - initialTime));
+//              initialTime = System.currentTimeMillis();
+//            }
+            break;
+          }
+        }
+      }
+      subSocket.close();
+    }
+  }
+
 }
